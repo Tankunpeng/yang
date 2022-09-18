f9297d3570f38bc51362085f8b9081ff9d4fcef1
tankunpeng <tankunpeng@live.cn>
2022-09-18 18:12:12 +0800

    docs: 添加注释

--- src/model/resolution.ts
+++ src/model/resolution.ts
@@ -1,25 +1,41 @@
 import {Cell, CellName} from "../cellName";
 
+/**
+ * 是否有解
+* */
 export function hasResolution(cells: Cell[]) {
     return !!filterCount(cells, ([name, count]) => count > 2).size
 }
 
+
+/**
+ * 未出现的卡片
+ * */
 export function missCell(cells: Cell[]) {
     return filterCount(cells, ([name, count]) => count === 0 && name !== CellName.blank)
 }
 
+/**
+ * 按数量过滤卡片
+ * */
 function filterCount(cells: Cell[], assert: ([name, count]: [string, number]) => boolean) {
     const count = countCell(cells)
     return new Set([...count].filter(assert).map(([name]) => name))
 }
 
+
+/**
+ * 统计各类卡片的数量
+ * */
 export function countCell(cells: Cell[]) {
     const cellMap = new Map(Object.values(CellName).map(key => [key, 0]))
     cells.forEach(cell => cellMap.set(cell.name, (cellMap.get(cell.name) || 0) + 1))
     return cellMap
 }
 
-// 假设每一层没有重叠
+/**
+ * 获取当前堆叠的表层（可立即选中的）卡片
+ * */
 export function getFaceCells(layers: Cell[][]) {
     const slice = 8
     const rows = 8
@@ -56,6 +72,9 @@ export function faceCellIdsSet(cells: Cell[][]) {
     return new Set(getFaceCells(cells).map(item => item.id))
 }
 
+/**
+ * 根据堆叠结构为 Cell 设置唯一 id
+ * */
 export const genId = (data: Cell[][]) => {
     return data.map((layer, stackIndex) => layer.map((cell, index) => {
         cell.id = cell.id || `${stackIndex + 1}-${index + 1}`
@@ -63,6 +82,10 @@ export const genId = (data: Cell[][]) => {
     }))
 }
 
+
+/**
+ * 卡片选中消除逻辑
+ * */
 export function addToHome(cells: Cell[], item: Cell) {
     cells = [...cells]
     if (cells.length > 6) {
